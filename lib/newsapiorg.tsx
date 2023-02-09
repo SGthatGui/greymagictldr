4ac998e5c740924aa04aced2b7874c62c72a212a
Pavlo Omelianchuk <pavlo-omelianchuk>
2023-02-09 15:33:57 +0000

    api call

--- lib/newsapiorg.tsx
+++ lib/newsapiorg.tsx
@@ -1,6 +1,16 @@
 export const newsorgsearchcall = async () => {
-  const searchlist = await fetch(
-    'https://newsapi.org/v2/everything?q=apple&apiKey=bef248401f3848159f196f961915b756'
-  ).catch((err) => console.log(err))
-  console.log(searchlist)
-}
+  // const searchlist = await fetch(
+  //   'https://newsapi.org/v2/everything?q=apple&apiKey=bef248401f3848159f196f961915b756'
+  // ).catch((err) => console.log(err))
+  // console.log(searchlist)
+
+  const res = await fetch(
+    `https://newsapi.org/v2/everything?q=Apple&searchIn=title&language=en&ensortBy=publishedAt&pageSize=3&apiKey=bef248401f3848159f196f961915b756`,
+    {
+      method: "GET",
+    }
+  );
+
+  console.log(await res.json());
+  return res;
+};
